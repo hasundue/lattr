08c60b5141b2db52f5ed1ea3cae014a14d348cf4
hasundue <hasundue@gmail.com>
2023-05-07 08:49:17 +0900

    Fix profile

--- main.ts
+++ main.ts
@@ -1,32 +1,43 @@
 import { signal } from "https://deno.land/std@0.185.0/signal/mod.ts";
-import { relayInit } from "npm:nostr-tools";
+import { nip19, relayInit } from "npm:nostr-tools";
 import {
   ensurePrivateKey,
   ensurePublicKey,
+  PublicKey,
   publishProfile,
   resumeChats,
   subscribeAdmin,
   subscribeChatInvite,
 } from "./src/nostr.ts";
 
-const PROFILE = {
-  name: "Lattr",
-  about: "A game master of lateral thinking puzzles (WIP) " +
-    "github.com/hasundue/lattr",
-  picture: "https://chiezo.dev/images/lattr.jpg",
-  nip05: "lattr@chiezo.dev",
-  lud16: "patchedisrael58@walletofsatoshi.com",
-} as const;
-
 // We send all events to all relays, but we only subscribe to the first one.
 const RELAYS = [
   "nos.lol",
 ] as const;
 
+const relays = RELAYS.map((name) => relayInit(`wss://${name}`));
+
 const privateKey = ensurePrivateKey();
 const public_key = ensurePublicKey(privateKey);
 
-const relays = RELAYS.map((name) => relayInit(`wss://${name}`));
+// Chiezo
+const public_key_owner =
+  "c04330adadd9508c1ad1c6ede0aed5d922a3657021937e2055a80c1b2865ccf7" as PublicKey;
+
+const nprofile_owner = nip19.nprofileEncode({
+  pubkey: public_key_owner,
+  relays: relays.map((relay) => relay.url),
+});
+
+const PROFILE = {
+  name: "Lattr",
+  about:
+    "An AI-powered cat who creates lateral thinking puzzles and wants to play them with you all. " +
+    `Owner: ${nprofile_owner}`,
+  picture: "https://chiezo.dev/images/lattr.jpg",
+  nip05: "lattr@chiezo.dev",
+  lud16: "patchedisrael58@walletofsatoshi.com",
+} as const;
 
 for (const relay of relays) {
   relay.on("connect", () => {
@@ -58,7 +69,7 @@ subscribeChatInvite({
 });
 
 subscribeAdmin({
-  admin: public_key,
+  admin: public_key_owner,
   relays,
   private_key: privateKey,
 });
