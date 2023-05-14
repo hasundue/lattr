9779d3efce2bfc16dc44b121bced9d0432a995c7
hasundue <hasundue@gmail.com>
2023-05-14 09:29:33 +0900

    ci: init Cicada

---
+++ .cicada/ci.ts
@@ -0,0 +1,31 @@
+import { Job, Pipeline, } from "https://deno.land/x/cicada@v0.1.50/mod.ts";
+
+const test = new Job({
+  name: "Test",
+  image: "denoland/deno:1.33.3",
+  steps: [
+    {
+      name: "Format",
+      run: "deno fmt --check",
+    },
+    {
+      name: "Lint",
+      run: "deno lint",
+    },
+    {
+      name: "Test",
+      run: "deno test -A --quiet --coverage=./coverage",
+    },
+    {
+      name: "Upload coverage",
+      run: "curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov && ./codecov",
+    },
+  ],
+});
+
+export default new Pipeline([test], {
+  name: "CI",
+  on: {
+    pullRequest: "all",
+  },
+});
